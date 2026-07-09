b05fd739aa52110c43a80e79244c3c0840bf4766
sohansingh11 <singh.sohan0011@gmail.com>
2026-07-09 12:14:32 +0530

    third

--- src/controllers/customBuilderController.js
+++ src/controllers/customBuilderController.js
@@ -1,17 +1,27 @@
 const crypto = require('crypto');
-const { composeSingleGear, composePreview } = require('../services/composeService');
 const { uploadBufferToCloudinary } = require('../utils/cloudinaryUpload');
+
+function dataUrlToBuffer(dataUrl, fieldName = 'image') {
+  if (!dataUrl || typeof dataUrl !== 'string') {
+    throw new Error(`${fieldName} is required.`);
+  }
+
+  const match = dataUrl.match(/^data:image\/[a-zA-Z0-9.+-]+;base64,(.+)$/);
+  if (!match) {
+    throw new Error(`${fieldName} must be a valid base64 image data URL.`);
+  }
+
+  return Buffer.from(match[1], 'base64');
+}
 
 exports.composeGearImages = async (req, res, next) => {
   try {
     const {
       section_id,
       uploaded_image,
-      silhouette_mask,
-      text_items,
-      effect,
       gear_1,
       gear_2,
+      preview_image,
       product
     } = req.body || {};
 
@@ -22,49 +32,37 @@ exports.composeGearImages = async (req, res, next) => {
       });
     }
 
-    if (!gear_1 || !gear_2) {
+    if (!gear_1 || !gear_1.composed_image) {
       return res.status(422).json({
         success: false,
-        message: 'gear_1 and gear_2 are required.'
+        message: 'gear_1.composed_image is required.'
       });
     }
 
-    if (!gear_1.base_image || !gear_2.base_image) {
+    if (!gear_2 || !gear_2.composed_image) {
       return res.status(422).json({
         success: false,
-        message: 'gear_1.base_image and gear_2.base_image are required.'
+        message: 'gear_2.composed_image is required.'
+      });
+    }
+
+    if (!preview_image) {
+      return res.status(422).json({
+        success: false,
+        message: 'preview_image is required.'
       });
     }
 
     const hash = crypto.randomBytes(6).toString('hex');
     const folder = `custom-builder/${product?.handle || 'builder'}/${hash}`;
 
-    /*
-      IMPORTANT:
-      baseImageDataUrl = PRODUCT MOCKUP IMAGE
-      overlayImageDataUrl = USER UPLOADED IMAGE
-    */
-    const gear1Buffer = await composeSingleGear({
-      baseImageDataUrl: gear_1.base_image,
-      overlayImageDataUrl: gear_1.overlay_image || uploaded_image,
-      gearConfig: gear_1,
-      textItems: text_items,
-      effect,
-      silhouetteMaskDataUrl: silhouette_mask
-    });
+    const uploadedImageBuffer = dataUrlToBuffer(uploaded_image, 'uploaded_image');
+    const gear1Buffer = dataUrlToBuffer(gear_1.composed_image, 'gear_1.composed_image');
+    const gear2Buffer = dataUrlToBuffer(gear_2.composed_image, 'gear_2.composed_image');
+    const previewBuffer = dataUrlToBuffer(preview_image, 'preview_image');
 
-    const gear2Buffer = await composeSingleGear({
-      baseImageDataUrl: gear_2.base_image,
-      overlayImageDataUrl: gear_2.overlay_image || uploaded_image,
-      gearConfig: gear_2,
-      textItems: text_items,
-      effect,
-      silhouetteMaskDataUrl: silhouette_mask
-    });
-
-    const previewBuffer = await composePreview(gear1Buffer, gear2Buffer);
-
-    const [gear1Upload, gear2Upload, previewUpload] = await Promise.all([
+    const [uploadedImageUpload, gear1Upload, gear2Upload, previewUpload] = await Promise.all([
+      uploadBufferToCloudinary(uploadedImageBuffer, folder, 'uploaded-image'),
       uploadBufferToCloudinary(gear1Buffer, folder, 'gear-1'),
       uploadBufferToCloudinary(gear2Buffer, folder, 'gear-2'),
       uploadBufferToCloudinary(previewBuffer, folder, 'preview')
@@ -72,9 +70,10 @@ exports.composeGearImages = async (req, res, next) => {
 
     return res.json({
       success: true,
-      message: 'Custom gear images generated successfully.',
+      message: 'Custom gear images uploaded successfully.',
       section_id: section_id || null,
       product: product || null,
+      uploaded_image: uploadedImageUpload.secure_url,
       gear_1_image: gear1Upload.secure_url,
       gear_2_image: gear2Upload.secure_url,
       preview_image: previewUpload.secure_url
